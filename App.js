84063aa12703a582c9e4465366c47e4ee55c1e06
SerryJohns <serembaj@gmail.com>
2018-05-03 00:16:18 +0100

    Create navigation tabs

--- App.js
+++ App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import Login from "./components/Login";
 import {StackNavigator} from "react-navigation";
 import Dashboard from "./components/Dashboard";
+import {colors} from "./resources/colors";
 
 export default class App extends Component {
   render() {
       const App = StackNavigator({
-          Home: { screen: Login },
+          Login: { screen: Login },
           Dashboard: { screen: Dashboard }
       }, {
-          headerMode: 'screen'
+          headerMode: 'screen',
+          initialRouteName: 'Dashboard',
+          navigationOptions: {
+              headerStyle: {
+                  backgroundColor: colors.colorPrimary
+              },
+              headerTintColor: '#ffffff'
+          }
       });
+
     return (
         <App />
     );
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
-import {Container, Content, Footer, Text} from "native-base";
+import {Body, Button, Container, Content, Header, Icon, Left, Right, Tab, Text, Title} from "native-base";
+import {Tabs} from "native-base/src/index";
 
 export default class Dashboard extends Component {
+    static navigationOptions = {
+        title: 'Newsfeed',
+        headerLeft: null,
+        header: (
+            <Header hasTabs>
+                <Left>
+                    <Button transparent>
+                        <Icon name="menu" />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Newsfeed</Title>
+                </Body>
+                <Right>
+                    <Button transparent>
+                        <Icon name="ios-search" />
+                    </Button>
+                    <Button transparent>
+                        <Icon name='md-more' />
+                    </Button>
+                </Right>
+            </Header>
+        )
+    };
+
     render() {
         return (
             <Container>
-                <Content padder>
-                    <Text>
-                        Dashboard
-                    </Text>
-                </Content>
+                <Tabs initialPage={0}>
+                    <Tab heading='Global News'>
+                        <Text>Some Global news</Text>
+                    </Tab>
+                    <Tab heading='Sports News'>
+                        <Text>Sports News</Text>
+                    </Tab>
+                    <Tab heading='Technology'>
+                        <Text>Tech News</Text>
+                    </Tab>
+                </Tabs>
             </Container>
         );
     }
