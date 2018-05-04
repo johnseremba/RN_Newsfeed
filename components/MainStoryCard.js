c8722dabf8b302130bac7b9641ea1c5654a0fea6
SerryJohns <serembaj@gmail.com>
2018-05-04 13:46:08 +0100

    Make MainStory card clickable

--- components/MainStoryCard.js
+++ components/MainStoryCard.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
 import {Card, CardItem, Text, View} from "native-base";
-import {Image, ImageBackground, StyleSheet} from "react-native";
+import {Image, ImageBackground, StyleSheet, TouchableHighlight} from "react-native";
+import {ANIMATIONS_SLIDE, CustomTabs} from "react-native-custom-tabs";
 
 export default class MainStoryCard extends Component {
+
+    openArticleUrl(url = "https://www.google.com") {
+        CustomTabs.openURL(url, {
+            toolbarColor: '#607D8B',
+            enableUrlBarHiding: true,
+            showPageTitle: true,
+            enableDefaultShare: true,
+            animations: ANIMATIONS_SLIDE
+        });
+    }
+
     render() {
         const title = "Messi takes on Ronaldo";
         const desc = `${'Sed ut perspiciatis unde omnis isle natus error sit voluptatem accusantiu doloremque laudantium, totam'}`;
         return (
             <Card>
-                <CardItem cardBody>
-                    <ImageBackground
-                        imageStyle={{resizeMode: 'cover'}}
-                        style={styles.bgImage}
-                        source={require('../img/bg.png')}>
-                        <View style={styles.container}>
-                            <View style={styles.contentBg}>
-                                <Text style={styles.header}>
-                                    {title}
-                                </Text>
-                                <Text note numberOfLines={2} style={{color: '#ffffff'}}>
-                                    {desc}
-                                </Text>
+                <TouchableHighlight onPress={() => this.openArticleUrl()}>
+                    <CardItem cardBody>
+                        <ImageBackground
+                            imageStyle={{resizeMode: 'cover'}}
+                            style={styles.bgImage}
+                            source={require('../img/bg.png')}>
+                            <View style={styles.container}>
+                                <View style={styles.contentBg}>
+                                    <Text style={styles.header}>
+                                        {title}
+                                    </Text>
+                                    <Text note numberOfLines={2} style={{color: '#ffffff'}}>
+                                        {desc}
+                                    </Text>
+                                </View>
                             </View>
-                        </View>
-                    </ImageBackground>
-                </CardItem>
+                        </ImageBackground>
+                    </CardItem>
+                </TouchableHighlight>
             </Card>
         );
     }
