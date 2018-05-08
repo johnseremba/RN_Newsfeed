97a713aaeefb70bf67b0f59ea1d476ee3eef54a9
SerryJohns <serembaj@gmail.com>
2018-05-08 15:14:11 +0100

    iOS React Native Firebase configuaration

--- android/app/src/main/java/com/newsfeed/MainApplication.java
+++ android/app/src/main/java/com/newsfeed/MainApplication.java
@@ -28,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RNFirebasePackage(),
             new CustomTabsPackage(),
             new LinearGradientPackage()
       );
